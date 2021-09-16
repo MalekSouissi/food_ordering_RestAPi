2f90b637faf128451b0e9dfbe7d3f1f89b1890b3
malek <maleksouissi751@gmail.com>
2021-09-17 01:26:21 +0200

    update

--- app/routes/order.routes.js
+++ app/routes/order.routes.js
@@ -4,10 +4,10 @@ module.exports = app => {
     var router = require("express").Router();
 
     router.post("/", orders.createOrder);
+        router.get("/getOrders", orders.findOrders);
     router.post("/add/:id", orders.newOrder);
     router.post("/updateOrder/:id/:itemId", orders.deleteOrderItem);
     router.post("/addOrderItem/:id/:itemId", orders.addOrderItem);
-    router.get("/getOrders", orders.findOrders);
     router.get("/getOrder/:id", orders.findOrder);
     router.get("/getUserOrder/:id", orders.findOrderByUserID);
     router.put("/order/:id", orders.update);
